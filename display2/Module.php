42adf8c0407706e41ec7fec2b98bd6575241902d
Pavel <pavlinter@gmail.com>
2017-12-31 13:23:37 +0100

    v2.1.4

--- display2/Module.php
+++ display2/Module.php
@@ -3,7 +3,7 @@
 /**
  * @copyright Copyright &copy; Pavels Radajevs, 2015
  * @package yii2-display-image2
- * @version 2.1.3
+ * @version 2.1.4
  */
 
 namespace pavlinter\display2;
@@ -16,7 +16,7 @@ use yii\base\InvalidConfigException;
  */
 class Module extends \yii\base\Module
 {
-    const VERSION = '2.1.3';
+    const VERSION = '2.1.4';
 
     public $componentId = 'display';
 
